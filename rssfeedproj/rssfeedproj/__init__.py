98c7996b2d3c284a6e60bd490609cf9606941336
thebugskiller <m.amir.dev7@gmail.com>
2024-10-02 14:35:51 -0400

    Cron Job to extract data from RSS URL

---
+++ rssfeedproj/rssfeedproj/__init__.py
@@ -0,0 +1,6 @@
+"""
+Init File
+"""
+from .celery import app as celery_app
+
+__all__ = ['celery_app']
---
+++ rssfeedproj/rssfeedproj/celery.py
@@ -0,0 +1,11 @@
+"""
+Celery
+"""
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rssfeedproj.settings')
+
+app = Celery('rssfeedproj')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
